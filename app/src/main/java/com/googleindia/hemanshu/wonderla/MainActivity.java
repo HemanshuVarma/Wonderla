384ed40c3cc490345a48922ba714ec6a275d2147
Hemanshu Varma <hemanshuvarma916@gmail.com>
2018-06-08 14:58:37 +0530

    Initial Commit

---
+++ app/src/main/java/com/googleindia/hemanshu/wonderla/MainActivity.java
@@ -0,0 +1,19 @@
+package com.googleindia.hemanshu.wonderla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void message(View view) {
+        Toast.makeText(MainActivity.this, R.string.message, Toast.LENGTH_SHORT).show();
+    }
+}
